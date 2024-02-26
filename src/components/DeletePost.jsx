743aedc05e8b9eb50b81908f14304d30361c5b91
Tukang <=>
2024-02-26 19:35:28 +0700

    DeletePost

--- src/components/DeletePost.jsx
+++ src/components/DeletePost.jsx
@@ -1,19 +1,56 @@
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+import { getPosts } from '../utils/fetchData';
+import { useArticle } from './ArticleContext';
+import { useParams } from 'react-router-dom';
+
 export default function DeletePost() {
+    const { setPosts } = useArticle();
+    const { id } = useParams();
+
+    const handleDelete = () => {
+        fetch(`https://blog-fe-batch5.neuversity.id/blog-fe-batch5/wp-json/wp/v2/posts/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                getPosts().then(data => {
+                    setPosts(data);
+                    document.getElementById('my_modal_3').close();
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Post deleted successfully.',
+                        icon: 'success'
+                    });
+                });
+            })
+            .catch(error => {
+                console.error('Error deleting post:', error);
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Failed to delete post.',
+                    icon: 'error'
+                });
+            });
+    };
 
     return (
         <div className="">
             {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <button type="button" className=" text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => document.getElementById('my_modal_3').showModal()}>Delete</button>
+            <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => document.getElementById('my_modal_3').showModal()}>Delete</button>
             <dialog id="my_modal_3" className="modal outline-none backdrop-blur-md">
                 <div className="modal-box outline-none">
-                    <h3 className="font-bold text-3xl flex justify-center pb-5">Are you sure you want to delete this post ??</h3> {/* Display the dynamic title */}
-                    <div className="modal-action flex justify-between">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn">Yes</button>
-                            <button className="btn">No</button>
-                        </form>
-                    </div>
+                    <form>
+                        <h3 className="font-bold text-3xl flex justify-center pb-5">Are you sure you want to delete this post ??</h3> {/* Display the dynamic title */}
+                        <div className="modal-action flex justify-between">
+                            <button type='button' className="btn" onClick={handleDelete}>Yes</button>
+                            <button className="btn" onClick={() => document.getElementById('my_modal_3').close()}>No</button>
+                        </div>
+                    </form>
                 </div>
             </dialog>
         </div>
--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -51,34 +51,35 @@ export default function EditPost() {
                     icon: 'error'
                 });
             });
-    }
-    return (
-        <div className="w-6">
-            <button className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => document.getElementById('my_modal_1').showModal()}>Edit</button>
-            <dialog id="my_modal_1" className="modal outline-none backdrop-blur-md">
-                <div className="modal-box outline-none">
-                    <h3 className="font-bold text-3xl flex justify-center pb-5">Edit Post</h3>
-                    <div className="mb-5">
-                        <form onSubmit={handleSubmit}>
-                            <label htmlFor="title" className="block mb-2 text-sm font-medium text-black dark:text-black">Title</label>
-                            <input
-                                type="text"
-                                id="title"
-                                className="border bg-gray-200 text-sm rounded-xl focus:ring-gray-200 focus:border-gray-200 block w-full p-2.5"
-                                placeholder="Tulis Judul Disini"
-                                value={title}
-                                onChange={(event) => setTitle(event.target.value)}
-                                required
-                            />
-                            <TextAreaArticle value={content} onChange={(event) => setContent(event.target.value)} />
-                            <ButtonUpload />
-                            <div className="modal-action">
-                                <button className="btn" onClick={() => document.getElementById('my_modal_2').close()}>Close</button>
-                            </div>
-                        </form>
+        }
+        return (
+            <div className="w-6">
+                <button className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => document.getElementById('my_modal_1').showModal()}>Edit</button>
+                <dialog id="my_modal_1" className="modal outline-none backdrop-blur-md">
+                    <div className="modal-box outline-none">
+                        <h3 className="font-bold text-3xl flex justify-center pb-5">Edit Post</h3>
+                        <div className="mb-5">
+                            <form onSubmit={handleSubmit}>
+                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-black dark:text-black">Title</label>
+                                <input
+                                    type="text"
+                                    id="title"
+                                    className="border bg-gray-200 text-sm rounded-xl focus:ring-gray-200 focus:border-gray-200 block w-full p-2.5"
+                                    placeholder="Tulis Judul Disini"
+                                    value={title}
+                                    onChange={(event) => setTitle(event.target.value)}
+                                    required
+                                />
+                                <TextAreaArticle value={content} onChange={(event) => setContent(event.target.value)} />
+                                <ButtonUpload />
+                                <div className="modal-action">
+                                    <button className="btn" onClick={() => document.getElementById('my_modal_2').close()}>Close</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </dialog>
-        </div>
-    )
-}
+                </dialog>
+            </div>
+        )
+    }
+
