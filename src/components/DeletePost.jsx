73762f06aab307f723a8dce3d4e22c4a2645bd83
Tukang <=>
2024-03-20 19:58:25 +0700

    Ngefix Delete Post

--- src/components/DeletePost.jsx
+++ src/components/DeletePost.jsx
@@ -39,8 +39,8 @@ export default function DeletePost({ id }) {
     return (
         <div className="">
             {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => document.getElementById('modal_delete').showModal()}>Delete</button>
-            <dialog id="modal_delete" className="modal outline-none backdrop-blur-md">
+            <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xs px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => document.getElementById('modal_delete' + id).showModal()}>Delete</button>
+            <dialog id={"modal_delete" + id} className="modal outline-none backdrop-blur-md">
                 <div className="modal-box outline-none">
                     <form>
                         <h3 className="font-bold text-3xl flex justify-center pb-5">Are you sure you want to delete this post ??</h3> {/* Display the dynamic title */}
