f5e2312cc14ba5b50813aea3dc2f77f13220dc3a
Tukang <=>
2024-03-20 18:58:14 +0700

    AddFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Anjing</div>
+    <footer className='bg-slate-700 text-white'>
+      <div className='container text-center py-5'>
+        <h1>Created By Tukang</h1>
+      </div>
+    </footer>
   )
 }
