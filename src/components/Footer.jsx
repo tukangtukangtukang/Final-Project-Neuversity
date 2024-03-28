cd92691eebb0073f465524c7eb5fece28f87b526
Tukang <=>
2024-03-28 15:18:09 +0700

    benerin footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className='bg-slate-700 text-white'>
+    <footer className='bg-slate-700 text-white fixed bottom-0 w-full'>
       <div className='container text-center py-5'>
         <h1>Created By Tukang</h1>
       </div>
     </footer>
-  )
+  );
 }
